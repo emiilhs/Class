437b1dd2fc5a2c49cd4b433851be15460ee386ff
unknown <ekviem1@gmail.com>
2023-05-05 06:36:18 +0400

    create

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    
+    public class Class1
+    {
+        public string ad;
+        public string soyad;
+        public int yas;
+        
+        public void fullname()
+        {
+            Console.WriteLine($"{ad} {soyad} registered");
+
+        }
+        public int age()
+        {
+            return yas;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp2;
+
+mesele mesel = new mesele();
+mesel.name = "Emil";
+mesel.surname = "Hwseynov";
+mesel.age = 23;
+mesel.ad();
+Console.WriteLine(mesel.yas());
+Console.WriteLine("--------------------------");
+Class1 klas = new Class1();
+klas.ad = "Emil";
+klas.soyad = "H";
+klas.yas = 23;
+klas.fullname();
+Console.WriteLine(klas.age());
+
+
+
+class mesele
+{
+    public string name;
+    public string surname;
+    public int age;
+    
+    public void ad()
+    {
+        Console.WriteLine($"{name} {surname} registered");
+
+    }
+
+    public int yas()
+    {
+        return age;
+    }
+
+}
