c6d11f859ad322963e3aada1cbc237e365cdae7d
emiilhs <128102441+emiilhs@users.noreply.github.com>
2023-05-06 22:11:23 +0400

    Update Class1.cs

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -1,25 +1,50 @@
 Hotel hotel1 = new Hotel();
-hotel1.id = 1;
 hotel1.HotelName = "23 Hotel";
-hotel1.Address = "Baki şəhəri, Nizami rayonu";
-hotel1.RoomPrice = 70;
-hotel1.RoomCount = 2;
+hotel1.Address = "Nizami rayonu";
+hotel1.RoomPrice = 100;
+Console.WriteLine(hotel1.GetPriceRoom(10));
 
-Console.WriteLine(hotel1.CalcRoomPrice());
+
 
 public class Hotel
 {
-    public int id;
-    public int RoomCount;
+    public int id { get; set; } = 0;
+    public int roomCount;
     public string HotelName;
     public string Address;
-    public int RoomPrice;
+    public int roomPrice;
 
-    public int CalcRoomPrice()
+    public int RoomPrice
     {
-        return RoomCount * RoomPrice;
+        get { return roomPrice; }
+        set
+        {
+            if (value < 60)
+            {
+                roomPrice = 0;
+            }
+            else if (value > 150)
+            {
+                roomPrice = 0;
+            }
+            else
+            {
+                roomPrice = value;
+            }
+        }
     }
 
-
-
+    public int GetPriceRoom(int CountOfRoom)
+    {
+        if (CountOfRoom < 0)
+        {
+            CountOfRoom = 0;
+        }
+        else if(CountOfRoom > 200) 
+        {
+            CountOfRoom = 0;
+        }
+        roomCount = CountOfRoom;
+        return roomCount * roomPrice;
+    }
 }
